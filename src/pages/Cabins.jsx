e431687a44b885c16f726e6ae35512358a8c3b53
chaitanya-ranjan29 <ranjanchaitanya29@gmail.com>
2023-10-31 10:51:28 +0530

    added supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getCabins } from '../services/apiCabins'
+import Row from '../ui/Row'
+import Heading from '../ui/Heading'
+import CabinTable from '../features/cabins/CabinTable'
 
 function Cabins() {
+
+  // useEffect(()=>{
+  //   getCabins().then(data=>console.log("cabin data: ", data));
+  // }, [])
+
   return (
-    <div>Cabins</div>
+    <>
+      <Row type='horizontal'>
+        <Heading as='h1' >All Cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <CabinTable/>
+    </>
   )
 }
 
---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://bxbsrlxqwzpstymxxhgs.supabase.co'
+// const supabaseKey = process.env.SUPABASE_KEY
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4YnNybHhxd3pwc3R5bXh4aGdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg2NDQ5NjYsImV4cCI6MjAxNDIyMDk2Nn0.oXrOMPqtcHaC9tsF0eMSYG-9NYykzD5jFmK6bLW2kwI"
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
